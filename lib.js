f6fef86f6d47873f4f26eb8891fb7dbcdc458140
nikhilpadiyath <nikhilpadiyath@gmail.com>
2022-11-21 08:13:15 +0530

    AddEventListeners added

--- lib.js
+++ lib.js
@@ -12,9 +12,14 @@ function addBookToLib(title,author,pages,read){
 }
 function displayBookOnPage(){
     const books= document.querySelector(".books");
+    const removeDivs = document.querySelectorAll(".card");
+    for ( let i=0; i<removeDivs.length; i++){
+        removeDivs[i].remove();
+    }
+
     myLibrary.forEach(myLibrary => {
         const card = document.createElement("div");
-        card.classList.add(".card");
+        card.classList.add("card");
         books.appendChild(card);
         for( let key in myLibrary){
             console.log(`${key}: ${myLibrary[key]}`);
@@ -25,7 +30,27 @@ function displayBookOnPage(){
     })
 }
 const addBookBtn= document.querySelector(".addbookbtn");
-addBookBtn.addEventListener("click", displayForm);
+addBookBtn.addEventListener("click",displayForm);
 function displayForm(){
     document.getElementById("add-book-form").style.display="";
+}
+const submitbtn= document.querySelector("#submit");
+submitbtn.addEventListener("click", intakeData);
+function intakeData(){
+    let title= document.getElementById("title").value;
+    let author= document.getElementById("author").value;
+    let pages= document.getElementById("pages").value;
+    let read= document.getElementById("read").value;
+
+    if((title == "") || (author =="") || (pages =="") ||(read == "")){
+        return;
+    }
+    addBookToLib(title,author,pages,read);
+    document.getElementById("add-book").reset();
+}
+
+const cleardata = document.querySelector("#reset");
+cleardata.addEventListener("click",clearForm);
+function clearForm(){
+    document.getElementById("add-book").reset();
 }
