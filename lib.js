b2469d0da00d87e9e2ebd87fd9bf58628c387355
nikhilpadiyath <nikhilpadiyath@gmail.com>
2023-01-22 14:25:41 +0530

    Read status change added

--- lib.js
+++ lib.js
@@ -36,6 +36,28 @@ function displayBookOnPage(){
             card.remove();
             displayBookOnPage();
         }
+        const readStatusButton= document.createElement("button");
+        readStatusButton.classList.add("read-status-btn");
+        readStatusButton.textContent= "Read/Not Read";
+        readStatusButton.dataset.linkedArray= index;
+        card.appendChild(readStatusButton);
+        readStatusButton.addEventListener("click",toggleReadStatus);
+        function toggleReadStatus(){
+            let getBookToToggle= readStatusButton.dataset.linkedArray;
+            Book.prototype= Object.create(Book.prototype);
+            const toggleBook= new Book();
+            if((myLibrary[parseInt(getBookToToggle)].Read)=="Yes"){
+                toggleBook.Read = "No";
+                myLibrary[parseInt(getBookToToggle)].Read = toggleBook.Read;
+                console.log("Book not read");
+            } else if ((myLibrary[parseInt(getBookToToggle)].Read)== "No"){
+                toggleBook.Read = "Yes";
+                myLibrary[parseInt(getBookToToggle)].Read = toggleBook.Read;
+                console.log("Book completed");
+            }
+            displayBookOnPage();
+        }
+
         for( let key in myLibrarys){
             console.log(`${key}: ${myLibrarys[key]}`);
             const para = document.createElement("p");
